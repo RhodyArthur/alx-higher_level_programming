8cffe7719ef777cdd7baa4bb1a8542133acb45c4
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-03-08 22:02:50 +0000

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,6 @@ def print_last_digit(number):
         last = number % 10
     else:
         temp = number * -1
-        last = (temp % 10) * -1
+        last = (temp % 10)
     print('{:d}'.format(last), end="")
     return last
