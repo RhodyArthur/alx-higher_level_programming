5cb5fe8f32f4594c0f23f04266bf17c9d63c8241
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-04-12 23:29:52 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+""" This module reads and print out a file """
+
 def read_file(filename=""):
-    """ Function that reads and print out a text file
-    
-    Args:
-        filename: filename
-        
-    Raises
-        Exception: when the file can be opened
+    """ Function takes in filename as argument
+    then output content of file to screen
     """
+    
     with open(filename, 'r', encoding="utf-8") as f:
         print("{}".format(f.read(), end="")
