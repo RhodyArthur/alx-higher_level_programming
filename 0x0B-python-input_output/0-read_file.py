869efea8f238ef2c37c0f294ca1b7cc1ec3b4e49
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-04-12 23:13:49 +0000

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """ Function that reads and print out a text file """
+    """ Function that reads and print out a text file
+    
+    Args:
+        filename: filename
+        
+    Raises
+        Exception: when the file can be opened
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         print("{}".format(f.read(), end="")
