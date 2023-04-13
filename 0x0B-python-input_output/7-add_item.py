32e930f81112f89344982f42bda2ad01793f7685
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-04-13 00:24:22 +0000

    add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" This module contains function
+that adds all args to a list """
+
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    
+    try:
+        my_obj = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_obj = []
+        my_obj.extend(sys.argv[1:])
+        save_to_json_file(my_obj, 'add_item.json')
