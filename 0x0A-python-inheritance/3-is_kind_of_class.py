7e93b8a3bb0e41ab4b3730de38a2504977e17722
RhodyArthur <rhodaarthur48@gmail.com>
2023-04-22 02:32:25 -0700

    is instance of

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module contains is_kind_of_function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ returns True if the object isinstance of """
+    return (isinstance(obj, a_class))
